f1fa997b02fcdf3c65258dda13022fd3a9e50e3a
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-10-09 10:02:45 +0200

    move connections to own header

--- src/include/horst/horst.h
+++ src/include/horst/horst.h
@@ -4,6 +4,7 @@
 
 #include <iostream>
 
+#include <horst/connection.h>
 #include <horst/unit.h>
 #include <horst/dbg.h>
 
@@ -16,37 +17,6 @@ namespace horst
 
     }
   }
-
-  struct connection 
-  {
-    const std::string m_from;
-    const std::string m_to;
-
-    connection
-    (
-      const std::string &from,
-      const std::string &to
-    ) :
-      m_from (from), m_to (to) 
-    {
-
-    }
-  };
-
-  struct connections 
-  {
-    std::vector<connection> m;
-
-    void add (const connection &c) 
-    {
-      m.push_back (c);
-    }
-
-    void add (const std::string &from, const std::string &to) 
-    {
-      m.push_back (connection (from, to));
-    }
-  };
 
   struct horst 
   {
