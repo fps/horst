e25249b58fcb4f503f3d908e8fd779278215fdd7
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-10-21 22:30:41 +0200

    add a timestamp; use __FILE_NAME__ instead of __FILE__ in debug macros

--- src/include/lv2_horst/dbg.h
+++ src/include/lv2_horst/dbg.h
@@ -1,9 +1,24 @@
 #pragma once
 
 #include <iostream>
+#include <ctime>
+
+namespace lv2_horst
+{
+  std::string timestamp ()
+  {
+    timespec tp;
+    clock_gettime (CLOCK_REALTIME, &tp);
+    
+    char buffer[64];
+    
+    sprintf(buffer, "%010ld.%03ld", tp.tv_sec, tp.tv_nsec / 1000000);
+    return buffer;
+  }
+}
 
 #ifdef HORST_DEBUG
-  #define DBG(x) { std::cerr << " [DBG] " << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << "(): " << x << std::endl << std::flush; }
+  #define DBG(x) { std::cerr << " [DBG] " << timestamp() << " " << __FILE_NAME__ << ":" << __LINE__ << " " << __FUNCTION__ << "(): " << x << std::endl << std::flush; }
 //#define DBG_JACK(x) { jack_info ("%s:%s:%s: %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, x); }
   #define DBG_JACK DBG
   #define DBG_ITEM(x) { std::cerr << " [DBG] " << x ; }
@@ -13,7 +28,7 @@
   #define DBG_ITEM(x) { }
 #endif
 
-#define INFO(x) { std::cerr << "[INFO] " << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << "(): " << x << std::endl << std::flush; }
+#define INFO(x) { std::cerr << "[INFO] " << timestamp() << " " << __FILE_NAME__ << ":" << __LINE__ << " " << __FUNCTION__ << "(): " << x << std::endl << std::flush; }
 
 #define DBG_ENTER DBG("...")
 #define DBG_EXIT DBG(".")
